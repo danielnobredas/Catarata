7dc659958f147ffadd9b2de8bd3cc8804d80e160
Daniel Nobre Da Silva <danielnobre@A307-31L.imd.local>
2017-11-06 20:24:03 -0300

    mudancas basicas em lerPpm

--- src/lerPpm.c
+++ src/lerPpm.c
@@ -3,5 +3,11 @@
 #include "mod.h"
 
 void lerPPM(FILE *imagem){
-	
+	int i,j;
+	char codigo[3];
+	int linha, coluna, tamanhoImg;
+	PPMImage ppm;
+	fscanf(imagem,"%s",codigo);
+	fscanf(imagem,"%d",)
+	for(i = 0; i < )
 }
