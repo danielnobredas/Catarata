829d5beede6bd1eee26acee213a501f0f5f877e2
Daniel Nobre Da Silva <danielnobre@A307-31L.imd.local>
2017-11-13 19:40:01 -0300

    leitura ppm

--- src/lerPpm.c
+++ src/lerPpm.c
@@ -5,9 +5,20 @@
 void lerPPM(FILE *imagem){
 	int i,j;
 	char codigo[3];
-	int linha, coluna, tamanhoImg;
-	PPMImage ppm;
+	int linha, coluna, tamanhoImg,intensidade;
+	PPMImage **ppm;
 	fscanf(imagem,"%s",codigo);
-	fscanf(imagem,"%d",)
-	for(i = 0; i < )
+	fscanf(imagem,"%d",&linha);
+	fscanf(imagem,"%d",&coluna);
+	fscanf(imagem,"%d",&intensidade);
+	printf("%s %d %d\n",codigo,linha,coluna );
+	ppm=(PPMImage**)calloc(linha,sizeof(PPMImage*));
+	for (int i = 0; i < linha; ++i) ppm[i]=(PPMImage*)calloc(coluna,sizeof(PPMImage));
+
+	for(i = 0; i < linha; i++){
+		for(j=0;j<coluna;j++){
+			fscanf(imagem,"%d %d %d",&(ppm[i])[j].pixel.R,&(ppm[i])[j].pixel.G,&(ppm[i])[j].pixel.B);
+			printf("%d\n %d\n %d\n",(ppm[i])[j].pixel.R,(ppm[i])[j].pixel.G,(ppm[i])[j].pixel.B);
+		}
+	}
 }
--- src/main.c
+++ src/main.c
@@ -61,7 +61,7 @@ int main(int argc, char **argv){
 	
 	
 	lerPPM(img);
-
+	fclose(img);
 	return 0;
 }
 
