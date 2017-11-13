b49a648dd05abde0978579e4abb8a02809b54ea2
Daniel Nobre Da Silva <danielnobre@A307-31L.imd.local>
2017-11-13 19:47:33 -0300

    saida ppm

--- src/lerPpm.c
+++ src/lerPpm.c
@@ -3,6 +3,7 @@
 #include "mod.h"
 
 void lerPPM(FILE *imagem){
+	FILE *saida=fopen("saida.ppm","w+");
 	int i,j;
 	char codigo[3];
 	int linha, coluna, tamanhoImg,intensidade;
@@ -18,7 +19,21 @@ void lerPPM(FILE *imagem){
 	for(i = 0; i < linha; i++){
 		for(j=0;j<coluna;j++){
 			fscanf(imagem,"%d %d %d",&(ppm[i])[j].pixel.R,&(ppm[i])[j].pixel.G,&(ppm[i])[j].pixel.B);
-			printf("%d\n %d\n %d\n",(ppm[i])[j].pixel.R,(ppm[i])[j].pixel.G,(ppm[i])[j].pixel.B);
+			// fprintf(saida,"%d\n %d\n %d\n",(ppm[i])[j].pixel.R,(ppm[i])[j].pixel.G,(ppm[i])[j].pixel.B);
+			
 		}
+
 	}
+	fprintf(saida, "%s\n",codigo );
+	fprintf(saida, "%d\n",linha );
+	fprintf(saida, "%d\n",coluna );
+	fprintf(saida, "%d\n",intensidade );
+	for(i = 0; i < linha; i++){
+		for(j=0;j<coluna;j++){
+			fprintf(saida,"%d\n%d\n%d\n",(ppm[i])[j].pixel.R,(ppm[i])[j].pixel.G,(ppm[i])[j].pixel.B);
+			
+		}
+
+	}
+
 }
