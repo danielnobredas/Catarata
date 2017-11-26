7bc6ee457990d91cd0ff5e64d3f1d797586edc2c
danielnobre <danielnobre2007@gmail.com>
2017-11-26 01:50:13 -0300

    pretoEbranco lido

--- src/lerPpm.c
+++ src/lerPpm.c
@@ -6,7 +6,7 @@ void lerPPM(FILE *imagem){
 	FILE *saida=fopen("saida.ppm","w+");
 	int i,j;
 	char codigo[3];
-	int linha, coluna,intensidade;
+	int linha, coluna,intensidade,pretoEbranco;
 	PPMImage **ppm;
 	fscanf(imagem,"%s",codigo);
 	fscanf(imagem,"%d",&linha);
@@ -23,16 +23,22 @@ void lerPPM(FILE *imagem){
 			
 		}
 	}
+
+//Saida imagem	
 	fprintf(saida, "%s\n",codigo );
 	fprintf(saida, "%d\n",linha );
 	fprintf(saida, "%d\n",coluna );
 	fprintf(saida, "%d\n",intensidade);
+
+
 	for(i = 0; i < linha; i++){
 		for(j=0;j<coluna;j++){
-			fprintf(saida,"%d\n%d\n%d\n",(ppm[i])[j].pixel.R,(ppm[i])[j].pixel.G,(ppm[i])[j].pixel.B);
-			
-		}
+			//Para a imagem ficar preto e branco foi utilizada o metodo passado no projeto
+			pretoEbranco=(ppm[i][j].pixel.R*0.3)+(ppm[i][j].pixel.G*0.59)+(ppm[i][j].pixel.B*0.11);
+			fprintf(saida,"%d\n%d\n%d\n",pretoEbranco,pretoEbranco,pretoEbranco);
 
+		}
+//# CREATOR: GIMP PNM Filter Version 1.1
 	}
 	fclose(saida);
 }
